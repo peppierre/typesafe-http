1ed66197e16716910952eefd4b6e9b2180c71fd1
peppierre <peppierre@gmail.com>
2025-07-17 21:32:12 +0200

    multiversion support extended, Angular v17 support added

---
+++ sandbox-v17/projects/sandbox-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true
+})
+export class AppComponent {
+  title = 'typed-http-example';
+}
